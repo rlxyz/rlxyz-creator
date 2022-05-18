e6a5782a980560c37b211750ecb1133bfcf8b7b5
jeevan.eth <33827489+jeevanpillay@users.noreply.github.com>
2022-05-18 13:45:45 +0800

    d

--- contracts/test/helpers/RhapsodyCreatorClaimRinkeby.sol
+++ contracts/test/helpers/RhapsodyCreatorClaimRinkeby.sol
@@ -6,7 +6,7 @@ import "../../RhapsodyCreatorClaim.sol";
 // @notice this RhapsodyCreatorClaimRinkeby is used in testnets such as rinkeby
 contract RhapsodyCreatorClaimRinkeby is RhapsodyCreatorClaim {
     /// @notice Semver Version
-    string public constant VERSION = "0.0.1";
+    string public constant VERSION = "0.0.2";
 
     constructor(uint256 _collectionSize) RhapsodyCreatorClaim("Rhapsody Creator Rinkeby", "RCT", _collectionSize) {}
 }
