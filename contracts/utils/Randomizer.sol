ec1f7e397531e94767a5237d37a46f668cb59f56
jeevan.eth <33827489+jeevanpillay@users.noreply.github.com>
2023-01-25 13:46:50 +1100

    removed Onwable in Randomizer

--- contracts/utils/Randomizer.sol
+++ contracts/utils/Randomizer.sol
@@ -2,10 +2,9 @@
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "./interfaces/IRandomizer.sol";
 
-contract Randomizer is IRandomizer, Ownable {
+contract Randomizer is IRandomizer {
     function getRandomValue() external view returns (uint256) {
         return uint256(blockhash(block.number - 1)) + 369963;
     }
