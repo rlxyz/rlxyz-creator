b8c8391474752ada54a2e339f91dd4be4c38a6b1
jeevan.eth <33827489+jeevanpillay@users.noreply.github.com>
2023-02-01 15:54:04 +1100

    added semantic versioming on Randomizer.sol

--- contracts/utils/Randomizer.sol
+++ contracts/utils/Randomizer.sol
@@ -5,6 +5,10 @@ pragma solidity ^0.8.0;
 import "./interfaces/IRandomizer.sol";
 
 contract Randomizer is IRandomizer {
+    /// ============ Semantic Versioning ============
+    /// @dev Semantic versioning for this contract
+    string public constant version = "1.0.0";
+
     function getRandomValue() external view returns (uint256) {
         return uint256(keccak256(abi.encodePacked(tx.origin, block.number - 69))) + 369963;
     }
